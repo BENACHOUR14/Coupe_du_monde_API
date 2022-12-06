b8d3a941b8f915049d5e35201744dd5eeb55127c
BENACHOUR14 <58175429+BENACHOUR14@users.noreply.github.com>
2022-12-06 14:54:31 +0100

    création de l application Teams

--- Teams/models.py
+++ Teams/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Teams(models.Model):
+    Groups = [
+            ('A', 'Groupe A'),
+            ('B', 'Groupe B'),
+            ('C', 'Groupe C'),
+            ('D', 'Groupe D'),
+            ('E', 'Groupe F'),
+            ('E', 'Groupe F'),
+            ('G', 'Groupe G'),
+            ('H', 'Groupe EH')
+            ]
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50,unique=True,null=False,blank=False)
+    group = models.CharField(max_length=1,choices=Groups,null=False,blank=False)
+    is_eliminated = models.BooleanField(default=True,null=False,blank=False)
