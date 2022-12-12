07bcae2ed26e445e3006b61d97f1e914ae14e4a9
BENACHOUR14 <58175429+BENACHOUR14@users.noreply.github.com>
2022-12-12 11:33:14 +0100

    required_changes

--- Games/models.py
+++ Games/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-import Teams
+from Teams.models import Teams
 import uuid
 
 # Create your models here.
@@ -7,10 +7,10 @@ import uuid
 class Games(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     date = models.DateTimeField()
-    isfinished = models.BooleanField(default=False,null=False,blank=False)
+    is_finished = models.BooleanField(default=False,null=False,blank=False)
     stadium = models.CharField(max_length=80,null=False,blank=False)
-    home_team = models.ForeignKey(Teams, null=False,blank=False)
-    away_team = models.ForeignKey(Teams, null=False,blank=False)
+    home_team = models.ForeignKey(Teams,on_delete=models.PROTECT, null=False,blank=False,related_name='home_team')
+    away_team = models.ForeignKey(Teams,on_delete=models.PROTECT, null=False,blank=False,related_name='away_team')
     goal_home_team  = models.PositiveSmallIntegerField(null=False,blank=False)
     goal_away_team = models.PositiveSmallIntegerField(null=False,blank=False)      
 
--- Teams/models.py
+++ Teams/models.py
@@ -3,15 +3,23 @@ import uuid
 
 
 class Teams(models.Model):
+    A = 'A'
+    B = 'B'
+    C = 'C'
+    D = 'D'
+    E = 'E'
+    F = 'F'
+    G = 'G'
+    H = 'H'
     Groups = [
-            ('A', 'Groupe A'),
-            ('B', 'Groupe B'),
-            ('C', 'Groupe C'),
-            ('D', 'Groupe D'),
-            ('E', 'Groupe F'),
-            ('E', 'Groupe F'),
-            ('G', 'Groupe G'),
-            ('H', 'Groupe EH')
+            (A, 'Groupe A'),
+            (B, 'Groupe B'),
+            (C, 'Groupe C'),
+            (D, 'Groupe D'),
+            (E, 'Groupe F'),
+            (F, 'Groupe F'),
+            (G, 'Groupe G'),
+            (H, 'Groupe EH')
             ]
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50,unique=True,null=False,blank=False)
