9dbc650131707008578cfb0845b37b4b9242170d
BENACHOUR14 <58175429+BENACHOUR14@users.noreply.github.com>
2022-12-12 15:05:40 +0100

    changes

--- Teams/models.py
+++ Teams/models.py
@@ -3,25 +3,17 @@ import uuid
 
 
 class Teams(models.Model):
-    A = 'A'
-    B = 'B'
-    C = 'C'
-    D = 'D'
-    E = 'E'
-    F = 'F'
-    G = 'G'
-    H = 'H'
-    Groups = [
-            (A, 'Groupe A'),
-            (B, 'Groupe B'),
-            (C, 'Groupe C'),
-            (D, 'Groupe D'),
-            (E, 'Groupe F'),
-            (F, 'Groupe F'),
-            (G, 'Groupe G'),
-            (H, 'Groupe EH')
-            ]
+    class Group(models.TextChoices):
+        A = 'A'
+        B = 'B'
+        C = 'C'
+        D = 'D'
+        E = 'E'
+        F = 'F'
+        G = 'G'
+        H = 'H'
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50,unique=True,null=False,blank=False)
-    group = models.CharField(max_length=1,choices=Groups,null=False,blank=False)
+    group = models.CharField(max_length=1,choices=Group.choices,null=False,blank=False)
     is_eliminated = models.BooleanField(default=True,null=False,blank=False)
