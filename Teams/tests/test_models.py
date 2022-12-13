7816293df54eef1ddcbc27e4089e205c16e8f1c2
BENACHOUR14 <58175429+BENACHOUR14@users.noreply.github.com>
2022-12-13 11:26:36 +0100

    last

--- Teams/tests/test_models.py
+++ Teams/tests/test_models.py
@@ -1,1 +1,27 @@
- 
+from django.test import TestCase
+from Teams.models import Teams
+import uuid
+from faker import Faker
+import pytest
+from django.db.utils import IntegrityError
+
+
+
+class TeamTestCase(TestCase):
+    def test_id_unicity(self):
+        
+        faker = Faker()
+        team1 = Teams.objects.create(id = uuid.uuid4(), name = faker.pystr(), group = Teams.Group.D)
+        with pytest.raises(IntegrityError):
+           Teams.objects.create(id = team1.id, name = faker.pystr(), group = Teams.Group.C)
+        
+    def test_name_length(self):
+        print("heelo")
+        faker = Faker()
+        team5 = Teams.objects.create(id = uuid.uuid4(), name = faker.pystr(min_chars=51, max_chars=60), group = 'Z')
+        print(team5.name)
+        print(len(team5.name))
+        print(team5.group)
+
+        
+
