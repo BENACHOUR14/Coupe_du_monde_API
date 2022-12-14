cbc8dcd6d6029a2af60a52c2c98a6d4a23e60fdd
BENACHOUR14 <58175429+BENACHOUR14@users.noreply.github.com>
2022-12-14 11:56:15 +0100

    update_1

--- Teams/management/commands/fixture_team.py
+++ Teams/management/commands/fixture_team.py
@@ -4,26 +4,15 @@ from Teams.models import Teams
 class Command(BaseCommand):
     
     def handle(self, *args, **kwargs):
-        help = 'Prepare the database for the project'
-        self.verify()
+        help = 'Insert four teams as fixtures'
         self.load_teams()
 
-        
-        
-       
-    def verify(self):
-        count = Teams.objects.count()
-        if(count == 0 ):
-            self.load_teams()
-        else:
-            print(str(count)+" Objects  exits in database")   
-        
     def load_teams(self):
         Teams.objects.all().delete()
-        Teams.objects.create(name='Maroc',group='H',is_eliminated = False)
-        Teams.objects.create(name='France',group='A',is_eliminated = False)
-        Teams.objects.create(name='Argentine',group='C',is_eliminated = False) 
-        Teams.objects.create(name='Croitie',group='D',is_eliminated = False)
+        Teams.objects.create(name='Maroc', group='H', is_eliminated=False)
+        Teams.objects.create(name='France', group='A', is_eliminated=False)
+        Teams.objects.create(name='Argentine', group='C', is_eliminated=False) 
+        Teams.objects.create(name='Croitie', group='D', is_eliminated=False)
         
          
         
