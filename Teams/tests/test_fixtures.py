dd92fff44c8fdade17a82b0e2b7a6a58b58cc349
BENACHOUR14 <58175429+BENACHOUR14@users.noreply.github.com>
2022-12-20 11:23:00 +0100

    team_fixture_test

---
+++ Teams/tests/test_fixtures.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from Teams.models import Teams
+from faker import Faker
+import pytest
+from django.db.utils import IntegrityError
+from django.core.management import call_command
+
+class TeamTestCase(TestCase):  
+
+        def test_existinig_data(self):     
+            faker = Faker()
+            team1 = Teams.objects.create(name=faker.pystr(), group=Teams.Group.A)
+            team2 = Teams.objects.create(name=faker.pystr(), group=Teams.Group.B)
+            team3 = Teams.objects.create(name=faker.pystr(), group=Teams.Group.C)
+            team4 = Teams.objects.create(name=faker.pystr(), group=Teams.Group.D)
+            team5 = Teams.objects.create(name=faker.pystr(), group=Teams.Group.D)
+            call_command('fixture_team')
+            self.assertEqual(Teams.objects.count(), 4)
+            self.assertTrue(Teams.objects.filter(name='Maroc').exists())
+            self.assertIn('Argentine',list(Teams.objects.all().values_list('name', flat=True)))
+
+            # self.assertIsNotNone(Teams.objects.get(name='France'))
+            # self.assertEqual([t.name for t in Teams.objects.all().order_by('name')],  ['Argentine','Croatie','France','Maroc'])
+            
+            
+            
+            
+            
+            
